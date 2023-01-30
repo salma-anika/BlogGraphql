76a2d026d00464c2a8b30e5930e127e992933b12
salma-anika <anikaavaa@gmail.com>
2023-01-30 21:17:59 +0600

    category

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import {useEffect , useState} from 'react'
+import moment from 'moment';
+import Link from 'next/link'
+import Categories from './Categories';
+import { getCategories } from '../services';
 
 const Categories = () => {
+    const [Categories, setCategories] = useState([]);
+  
+    useEffect(() => {
+     
+        setCategories().then((newCategories) => {
+            setCategories(newCategories);
+        });
+      
+    }, []);
+
   return (
     <div>
       
